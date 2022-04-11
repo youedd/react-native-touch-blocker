8520c1a8644ad9557dda44dff0946cf52d35e99a
youssef eddibili <youssef.eddibili@zenpark.com>
2022-04-11 12:10:32 +0200

    TouchBlocker Component

---
+++ src/TouchBlocker/TouchBlocker.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { PanResponder, View } from "react-native";
+import { TouchBlockerProps } from "./types";
+
+const panResponder = PanResponder.create({
+  onPanResponderTerminationRequest: () => false,
+  onStartShouldSetPanResponderCapture: () => true,
+  onShouldBlockNativeResponder: () => true,
+});
+
+const TouchBlocker: React.FC<TouchBlockerProps> = ({
+  enableTouchEvents,
+  ...props
+}) => {
+  const panHanlders = enableTouchEvents ? {} : panResponder.panHandlers;
+  return <View {...props} {...panHanlders} />;
+};
+
+export default TouchBlocker;
---
+++ src/TouchBlocker/types.ts
@@ -0,0 +1,5 @@
+import { ViewProps } from "react-native";
+
+export type TouchBlockerProps = ViewProps & {
+  enableTouchEvents?: boolean;
+};
