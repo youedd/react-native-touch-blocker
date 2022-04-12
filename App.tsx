8c70197d22b00f82b91fc7f4b37f809debf2727a
youssef eddibili <youssef.eddibili@zenpark.com>
2022-04-12 18:59:59 +0200

    Improve Example

--- App.tsx
+++ App.tsx
@@ -1,25 +1,27 @@
 import React from 'react'
 import { Button, StyleSheet, View } from 'react-native'
-import TouchBlocker from './src/TouchBlocker'
+import TouchBlocker from './src'
 
 const App: React.FC = () => {
   const [touchEnabled, setTouchEnabled] = React.useState(true)
   const [background, setBackground] = React.useState('#FFFFFF')
+
+  const toggleTouch = (): void => {
+    setTouchEnabled((value) => !value)
+  }
+
+  const changeBackground = (): void => {
+    setBackground(randomColor())
+  }
+
   return (
     <View style={[Styles.container, { backgroundColor: background }]}>
       <Button
         title={touchEnabled ? 'Disable Touch' : 'Enable Touch'}
-        onPress={() => {
-          setTouchEnabled((value) => !value)
-        }}
+        onPress={toggleTouch}
       />
       <TouchBlocker enableTouchEvents={touchEnabled}>
-        <Button
-          title='Change background'
-          onPress={() => {
-            setBackground(randomColor())
-          }}
-        />
+        <Button title='Change background' onPress={changeBackground} />
       </TouchBlocker>
     </View>
   )
